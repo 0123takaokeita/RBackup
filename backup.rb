8412d357dc2cad5d8cb5480b5742ec4f7b6b13d1
Keita Takao <keita.takao.1112@gmail.com>
2021-12-09 22:10:27 +0900

    ref: 命名の修正と冗長箇所の書き換え

--- backup.rb
+++ backup.rb
@@ -1,19 +1,13 @@
-require "rubygems"
 require "date"
-require "./setting.rb"
+require_relative "./setting.rb"
 
 permalink = File.dirname(Dir.pwd)
-current_file_name = File.basename(permalink)
-output_file_path = "#{Dir.pwd}/zip/#{current_file_name}_#{$day}.zip"
-cmd = "cd #{permalink} ; zip -r #{output_file_path} . -x #{$no_zip}"
+target_dir_name = File.basename(permalink)
+output_file_path = "#{Dir.pwd}/zip/#{target_dir_name}_#{$day}.zip"
+
+# Zipの中のファイルは常にひとつが良いため掃除しています。
 zip_dir_array = Dir.glob("#{Dir.pwd}/zip/*")
+File.unlink(*zip_dir_array)
 
-#  zipフォルダを空にしたいため掃除します。
-def unlink_dir(dir_arr)
-  dir_arr.each do |file|
-    File.unlink(file)
-  end
-end
-
-unlink_dir(zip_dir_array)
+cmd = "cd #{permalink} ; zip -r #{output_file_path} . -x #{$no_zip}"
 exec(cmd)
